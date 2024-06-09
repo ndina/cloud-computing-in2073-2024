0e58eb20aaecd3d54e493116d4416ef0d94409d1
ndina <31282816+ndina@users.noreply.github.com>
2024-06-09 20:13:52 +0200

    exercise 3

--- api/books/get/main.go
+++ api/books/get/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/labstack/echo/v4"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive" 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
